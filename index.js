ec75323fa606d60c62aea0ecf74ccabce86dc325
corti7 <coa_777@hotmail.com>
2018-10-08 20:28:07 -0500

    Index - Hola Mundo

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Platzi-video!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById('app')
+const holaMundo = <h1>Hola Mundo - Estudiantes!</h1>
+
+//ReactDOM(que voy a renderizar, donde lo hare)
+ReactDOM.render(holaMundo, app)
