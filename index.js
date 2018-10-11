012afbd4247c1d92b6e83fdfc236b4d7c9937e4a
corti7 <coa_777@hotmail.com>
2018-10-10 23:10:54 -0500

    Enlazando eventos al DOM

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ import Media from './src/playlist/components/media';
 const container = document.getElementById('app')
 
 //ReactDOM(que voy a renderizar, donde lo hare)
-render(<Media />, app);
+render(<Media type="video" title="Que es Responsive Design?" author="cortiz" image="./images/covers/responsive.jpg" />, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component{
+  
+  handleClick = (event) => {
+    console.log(this.props.image)
+  }
   render() {
     const styles = {
       container: {
@@ -13,20 +18,27 @@ class Media extends Component{
       }
     }
     return (
-      <div className="Media">
+      <div className="Media" onClick={ this.handleClick }>
         <div className="Media-cover">
           <img className="Media-image"
-            src="./images/covers/bitcoin.jpg"
+            src={ this.props.image }
             alt=""
             width={260}
             height={160}
           />
-          <h3 className="Media-title">Por que aprender React..</h3>
-          <p className="Media-author">coa</p> 
+          <h3 className="Media-title">{ this.props.title }</h3>
+          <p className="Media-author">{ this.props.author }</p> 
         </div>
       </div>
       )
   }
+}
+
+Media.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  type: PropTypes.oneOf(['video', 'audio']),
 }
 
 export default Media;
