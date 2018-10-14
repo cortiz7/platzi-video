3fe524311d18cac2e781c34667a1c55125657066
corti7 <coa_777@hotmail.com>
2018-10-14 16:28:42 -0500

    Filtro de Busqueda

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -4,8 +4,19 @@ import './search.css';
 
 function Search(props){
   return(
-      <form action="" className="Search">
-        <input type="text" className="Search-input" placeholder="Busca tu video favorito" />
+      <form 
+        className="Search"
+        onSubmit={props.handleSubmit}
+      >
+        <input
+          ref={props.setRef} 
+          type="text" 
+          className="Search-input" 
+          placeholder="Busca tu video favorito"
+          name="search"
+          onChange={props.handleChange}
+          value={props.value}
+        />
       </form>
   )
 }
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import Search from '../components/search';
 
 class SearchContainer extends Component{
-  render() {
+  state = {
+    value:'Busqueda',
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.input.value, 'submit');
+  }
+  setInputRef = (element) => {
+    this.input = element;
+  }
+  handleInputChange = (event) => {
+    this.setState({
+      value:this.input.value.replace(' ', '-')
+    }) 
+  }
+  render() {    
     return(
-      <Search />
+      <Search
+        setRef={this.setInputRef}
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleInputChange}
+        value={this.state.value}
+      />
     )
   }
 }
