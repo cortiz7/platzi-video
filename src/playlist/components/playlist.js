4ce117f98c106da4a2dc812843b6a24c09c13f6c
corti7 <coa_777@hotmail.com>
2018-10-14 04:27:41 -0500

    Componente Play

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Media from './media.js';
+import Play from '../../icons/components/play.js';
 
 function Playlist(props) {
   const playlist = props.data.categories[0].playlist;
   console.log(props.data);  
   return (
     <div className="Playlist">
+      <Play 
+        size={100}
+        color="red"
+      />        
       {
         playlist.map((item) => {
             return <Media {...item} key={item.id} />
