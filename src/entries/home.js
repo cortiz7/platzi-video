3b2a5b2635b8020ea43c674e2810b9a1b9fdc601
corti7 <coa_777@hotmail.com>
2018-10-14 11:51:52 -0500

    Modal Container

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-// import Playlist from '../playlist/components/playlist';
 import data from '../api.json';
 
-const container = document.getElementById('app')
+const homeContainer = document.getElementById('home-container')
 
 //ReactDOM(que voy a renderizar, donde lo hare)
-render(<Home data={data} />, app);
+render(<Home data={data} />, homeContainer);
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
+import Related from '../components/related';
+import ModalContainer from '../../widgets/containers/modal';
 
 class Home extends Component {
   render() {
     return (
-      <div>Home Entrie
-          <HomeLayout>
-             <Categories categories={this.props.data.categories} />
-          </HomeLayout>
+      <div>        
+        <HomeLayout>
+          <Related/>
+          <Categories categories={this.props.data.categories} />
+          <ModalContainer>
+            <h1>Esto es un portal</h1>  
+          </ModalContainer>  
+        </HomeLayout>
       </div>      
     )
   }
