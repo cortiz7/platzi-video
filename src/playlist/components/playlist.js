341e99cc2c882f701efdf839cb27c7e54fde811a
corti7 <coa_777@hotmail.com>
2018-10-14 04:43:40 -0500

    Componentes: volume, pause, fullscreen

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import Media from './media.js';
-import Play from '../../icons/components/play.js';
+import Media from './media';
+import Play from '../../icons/components/play';
+import FullScreen from '../../icons/components/full-screen';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
 
 function Playlist(props) {
   const playlist = props.data.categories[0].playlist;
@@ -10,7 +13,19 @@ function Playlist(props) {
       <Play 
         size={100}
         color="red"
-      />        
+      />
+      <FullScreen 
+        size={100}
+        color="red"
+      />
+      <Pause 
+        size={100}
+        color="red"
+      />
+      <Volume 
+        size={100}
+        color="red"
+      />         
       {
         playlist.map((item) => {
             return <Media {...item} key={item.id} />
